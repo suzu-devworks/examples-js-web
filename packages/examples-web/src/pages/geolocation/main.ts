c05fc12d8dd42fc8a095b11e7a76a9f18b3347dc
A.suzuki <suzu.devworks@gmail.com>
2025-09-10 13:23:57 +0000

    Add watch Geolocation example.

--- packages/examples-web/src/pages/geolocation/main.ts
+++ packages/examples-web/src/pages/geolocation/main.ts
@@ -7,10 +7,23 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <h2>Using the Geolocation API</h2>
     <p>If the object exists, geolocation services are available.</p>
     <section class="controls">
-      <button id="get-geolocation">Get</button>
+      <button id="get-geolocation">Get Map Link</button>
       <div>
         <a id="map-link-geolocation" href=""></a>
       </div>
+    </section>
+  </article>
+  <article>
+    <h2>Watching the current position</h2>
+    <p>If the position data changes (either by device movement or if more accurate geo information arrives),
+      you can set up a callback function that is called with that updated position information.</p>
+    <section class="controls">
+      <button id="watch-geolocation" data-watching="false" role="switch" aria-checked="false">
+        <span>Watch</span>
+      </button>
+    </section>
+    <section>
+      <output id="output-geolocation" ></output>
     </section>
   </article>
 `
@@ -33,5 +46,23 @@ document.addEventListener("DOMContentLoaded", function () {
       anchor.target = "_blank"
       anchor.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`
     })
+  })
+
+  const output = document.querySelector<HTMLElement>("#output-geolocation")!
+  let watchID: number
+  document.querySelector<HTMLButtonElement>("#watch-geolocation")!.addEventListener("click", function () {
+    if (this.dataset.watching === "false") {
+      watchID = navigator.geolocation.watchPosition((position) => {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        const now = new Date()
+        output.textContent = `Latitude: ${latitude}, Longitude: ${longitude} at ${now.toISOString()}`
+      })
+      this.dataset.watching = "true"
+    } else {
+      navigator.geolocation.clearWatch(watchID)
+      output.textContent = ""
+      this.dataset.watching = "false"
+    }
   })
 })
