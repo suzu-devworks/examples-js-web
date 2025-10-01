111991bc74ddb72ce919734d3faf31c36f8e9412
A.suzuki <suzu.devworks@gmail.com>
2025-10-01 13:52:28 +0000

    Update github pages root.

--- .github/deploy-static.sh
+++ .github/deploy-static.sh
@@ -10,6 +10,7 @@ OPTION='-avr'
 # rsync ${OPTION} --exclude ${EXCLUDE} ${SOURCE}/_root/ ${DIST}
 
 # static projects
+rsync ${OPTION} --exclude ${EXCLUDE} pages/index.html ${DIST}/
 rsync ${OPTION} --exclude ${EXCLUDE} ${SOURCE}/examples-web/dist/ ${DIST}/examples-web/
 rsync ${OPTION} --exclude ${EXCLUDE} ${SOURCE}/examples-pwa-camera/ ${DIST}/examples-pwa-camera/
 rsync ${OPTION} --exclude ${EXCLUDE} ${SOURCE}/examples-web-visualization/public/ ${DIST}/examples-web-visualization/
